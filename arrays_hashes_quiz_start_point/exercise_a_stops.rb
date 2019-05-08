1a5af3e3d57637bf8320f15ad067510bc51c399b
marcinsukacz <marcin.sukacz@gmail.com>
2019-05-08 16:56:59 +0100

    first ex completed

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -2,12 +2,33 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
 stops.push("Edinburgh Waverley")
+
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops.unshift("Glasgow Queen St")
+
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(4, "Polmont")
+
 # 4. Work out the index position of `"Linlithgow"`
+p stops.index("Linlithgow")
+
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
+
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
+
 # 7. How many stops there are in the array?
+p "#{stops.count()} stops in the array"
+
 # 8. How many ways can we return `"Falkirk High"` from the array?
+p stops[4]
+p stops.at(4)
+
 # 9. Reverse the positions of the stops in the array
+p stops.reverse
+
 # 10. Print out all the stops using a for loop
+for element in stops
+  p element
+end
